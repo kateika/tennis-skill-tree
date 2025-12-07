ac0956f82fd7e909098e6a7fad4a08e4d100107a
Kate Shastakova <kate.shestakova@me.com>
2025-12-06 19:20:06 -0800

    initial implementation of custom nodes and custom connection lines with a simple form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,102 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import React, { useCallback } from 'react';
 
-import Hello from '@components/Hello';
-import World from '@components/World';
+import {
+  Background,
+  ReactFlow,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  MarkerType,
+  Panel,
+} from '@xyflow/react';
 
-import './App.scss';
+import '@xyflow/react/dist/style.css';
 
-export const App = (): JSX.Element => (
-  <div className="componentWrapper">
-    <h2>Hello App</h2>
-    <BrowserRouter>
-      <nav className="routingWrapper">
-        <b>Routing: </b>
-        <Link to="/">Hello component</Link>
-        <Link to="/world">World component</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-        <Route path="/world" element={<World />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
-);
+import SkillNode from './skillNode/SkillNode';
+import Edge from './edge/Edge';
+import NodeConnectionLine from './nodeConnectionLine/NodeConnectionLine';
+import AddNodeForm from './addNodeForm/AddNodeForm';
+
+const initialNodes = [
+  {
+    id: '1',
+    type: 'custom',
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: '2',
+    type: 'custom',
+    position: { x: 150, y: 200 },
+  },
+  {
+    id: '3',
+    type: 'custom',
+    position: { x: 40, y: 100 },
+  },
+  {
+    id: '4',
+    type: 'custom',
+    position: { x: 200, y: 0 },
+  },
+];
+
+/*@ts-ignore */
+const initialEdges = [];
+
+const connectionLineStyle = {
+  stroke: '#b1b1b7',
+};
+
+const nodeTypes = {
+  custom: SkillNode,
+};
+
+const edgeTypes = {
+  floating: Edge,
+};
+
+const defaultEdgeOptions = {
+  type: 'floating',
+  markerEnd: {
+    type: MarkerType.ArrowClosed,
+    color: '#b1b1b7',
+  },
+};
+
+const EasyConnectExample = () => {
+  /*@ts-ignore */
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  /*@ts-ignore */
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    /*@ts-ignore */
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+        nodeTypes={nodeTypes}
+        /*@ts-ignore */
+        edgeTypes={edgeTypes}
+        defaultEdgeOptions={defaultEdgeOptions}
+        /*@ts-ignore */
+        connectionLineComponent={NodeConnectionLine}
+        connectionLineStyle={connectionLineStyle}
+      >
+        <Background />
+        <Panel><AddNodeForm /></Panel>
+      </ReactFlow>
+    </div>
+  );
+};
+
+export default EasyConnectExample;
---
+++ src/components/App/edge/Edge.tsx
@@ -0,0 +1,34 @@
+import { BaseEdge, getStraightPath, useInternalNode } from '@xyflow/react';
+
+import { getEdgeParams } from './edgeCalc.js';
+
+/*@ts-ignore */
+function Edge({ id, source, target, markerEnd, style }) {
+  const sourceNode = useInternalNode(source);
+  const targetNode = useInternalNode(target);
+
+  if (!sourceNode || !targetNode) {
+    return null;
+  }
+
+  const { sx, sy, tx, ty } = getEdgeParams(sourceNode, targetNode);
+
+  const [path] = getStraightPath({
+    sourceX: sx,
+    sourceY: sy,
+    targetX: tx,
+    targetY: ty,
+  });
+
+  return (
+    <BaseEdge
+      id={id}
+      className="react-flow__edge-path"
+      path={path}
+      markerEnd={markerEnd}
+      style={style}
+    />
+  );
+}
+
+export default Edge;
